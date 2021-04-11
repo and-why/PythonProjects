ff60ec518f6ad458713087fb84160dcd5680e8cc
Andy Smith <work@andysmith.is>
2021-04-12 09:52:17 +1000

    updated main.py

--- Mail Merge Project Start/main.py
+++ Mail Merge Project Start/main.py
@@ -1,19 +1,23 @@
-#TODO: Create a letter using starting_letter.txt 
+PLACEHOLDER = "[name]"
+
+
+#TODO: Create a letter using starting_letter.txt
 #for each name in invited_names.txt
-with open("./Input/Letters/starting_letter.txt") as letter:
-    letter = letter.read()
 
 with open("./Input/Names/invited_names.txt") as name_list:
     name_list = name_list.readlines()
 
-for name in name_list:
-    name = name.strip()
-    if " " in name:
-        file_name = name.replace(" ", "_").lower()
-    else:
-        file_name = name.lower()
-    with open(f"./Output/ReadyToSend/letter-to-{file_name}.txt", mode="w") as mail_merge:
-        mail_merge.write(letter.replace("[name]", name))
+with open("./Input/Letters/starting_letter.txt") as letter:
+    letter = letter.read()
+
+    for name in name_list:
+        name = name.strip()
+        if " " in name:
+            file_name = name.replace(" ", "_").lower()
+        else:
+            file_name = name.lower()
+        with open(f"./Output/ReadyToSend/letter-to-{file_name}.txt", mode="w") as mail_merged:
+            mail_merged.write(letter.replace(PLACEHOLDER, name))
 
 
 #Replace the [name] placeholder with the actual name.
