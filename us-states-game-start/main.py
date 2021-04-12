be72fdc246f40d8b05e116bd955cd2b2952e6525
Andy Smith <work@andysmith.is>
2021-04-13 08:39:19 +1000

    using list comprehension to refactor some code

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -14,7 +14,7 @@ turtle.shape(image)
 # turtle.onscreenclick(get_mouse_click_coor)
 # turtle.mainloop()
 data = pandas.read_csv("50_states.csv")
-states = data.state.to_list()
+all_states = data.state.to_list()
 
 states_guessed = []
 game_is_on = True
@@ -24,14 +24,15 @@ while len(states_guessed) < 50:
     answer_state = (screen.textinput(title=f"Guess the state. {len(states_guessed)}/50", prompt="What's another State's name?")).title()
 
     if answer_state == "Exit":
-        states_to_learn = []
-        for state in states:
-            if state not in states_guessed:
-                states_to_learn.append(state)
-            new_data = pandas.DataFrame(states_to_learn)
-            new_data.to_csv("states-to-learn.csv")
+        states_to_learn = [state for state in all_states if state not in states_guessed]
+        # states_to_learn = []
+        # for state in states:
+        #     if state not in states_guessed:
+        #         states_to_learn.append(state)
+        new_data = pandas.DataFrame(states_to_learn)
+        new_data.to_csv("states-to-learn.csv")
         break
-    for state_check in states:
+    for state_check in all_states:
         if answer_state == state_check:
             states_guessed.append(state_check)
             x_cor = int(data[data.state == state_check].x)
